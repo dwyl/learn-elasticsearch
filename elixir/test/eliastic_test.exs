98dfc3b3924167038db7916f73a10700e9aa0fc1
nelsonic <contact.nelsonic+github@gmail.com>
2017-03-20 21:14:49 +0000

    adds working test to GET a record from ElasticSearch! #20

--- elixir/test/eliastic_test.exs
+++ elixir/test/eliastic_test.exs
@@ -1,8 +1,16 @@
 defmodule ElasticTest do
   use ExUnit.Case
   doctest Elastic
+  import Tirexs.HTTP
 
   test "the truth" do
     assert 1 + 1 == 2
+  end
+
+  test "fetch Jane from ElasticSearch " do
+    {:ok, status, result} = get("/my_index/users/1")
+    expected = %{email: "jane@example.com", name: "Jane"}
+    # IO.inspect result._source
+    assert result._source == expected
   end
 end
